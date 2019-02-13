3c29731ed1d4df9374d2249a9162fe55904ca49c
Laurynas Leišis <lauris.leisis@gmail.com>
2019-02-13 18:21:55 +0200

    prideti vektoriai

--- 2_uzduotis.cpp
+++ 2_uzduotis.cpp
@@ -1,34 +1,60 @@
 #include <iostream>
 #include <string>
-#include <iomanip>  
-double Mediana(int ND[], int n);
+#include <iomanip>
+#include <sstream>
+double Mediana(int *ND, int n, int p);
 int main(){
 	std::string vardas, pavarde;
-	int n;
+	int n = -1;
     double egzaminas;
 	double galutinis;
 	double vidurkis;
+	int *ND = new int;
 	int suma = 0;
 	std::cout <<"Iveskite varda" << std::endl;
-	std::cin >> vardas;
+	std::getline(std::cin, vardas);
 	std::cout <<"Iveskite pavarde" << std::endl;
-	std::cin >> pavarde;
-	std::cout <<"Iveskite kiek namu darbu buvo atlikta" << std::endl;
-	std::cin >> n;
-	int ND[n+1];
+	std::getline(std::cin, pavarde);
+	std::cout <<"Iveskite kiek namu darbu buvo atlikta (0 = ivedimas baigiamas)" << std::endl;
+	//std::cin >> n;
+	int p = 0;
+	int laikina = 1;
 	int laik;
-	for(int i =1;i <=n; i++){
-		std::cout <<"Iveskite nr " << i << " namu darbo rezultata (1-10)" << std::endl;
+        std::string ivestis;
+        std::getline(std::cin, ivestis);
+        std::stringstream stream(ivestis);
+        
+    if(stream >> n){
+			for(int i =1;i <=n; i++){
+		std::cout <<"Iveskite nr " << i << " namu darbo rezultata (1-10) (0 = ivedimas baigiamas)" << std::endl;
 		std::cin >> laik;
-		if(laik >10 || laik <1){
+		if(laik >10 || laik <0){
 		  std::cout <<"Jus ivedete neimanoma rezultata, bandykite dar karta" << std::endl;
 		  i= i-1;
 		}
 		else {
-			ND[i] = laik;
+			*(ND+i) = laik;
 			suma = suma + laik;
 		}
 	}
+        }
+    else{
+        while(laikina != 0){
+			std::cout <<"Iveskite nr " << p+1 << " namu darbo rezultata (1-10) (0 = ivedimas baigiamas)" << std::endl;
+			std::cin>> laikina;
+			if(laikina >10 || laikina <0){
+		  		std::cout <<"Jus ivedete neimanoma rezultata, bandykite dar karta" << std::endl;
+		}
+			else {
+				p++;
+				*(ND+p) = laikina;
+				suma = suma + laikina;
+			}
+		}
+	}
+
+		
+
 	int ilgis;
 	int egzam;
 	double mediana;
@@ -42,17 +68,17 @@ int main(){
 		}
 	}
 	else egzaminas = egzam;
-	ND[n+1] = egzaminas;
-	vidurkis = (double)suma/n;
+	if(p>0)vidurkis = (double)suma/p;
+	else vidurkis = (double)suma/n;
 	galutinis = 0.4 * vidurkis + 0.6*egzaminas;
-	mediana = Mediana(ND, n);
+	mediana = Mediana(ND, n, p);
 	std::cout <<std::left <<std::setw(15)<<"Pavarde" <<std::left << std::setw(13) << "Vardas" <<std::left << std::setw(15) <<"VidGalutinis" << std::left << "MedGalutinis" <<std::endl;
 	std::cout <<std::setfill('-') << std::setw(53) << "-"<< std::endl;
 	std::cout << std::setfill(' ');
 	std::cout <<std::left <<std::setw(15)<< pavarde << std::left << std::setw(13)  << vardas <<std::left << std::setw(15) << std::setprecision(2) <<std::fixed << galutinis << std::left << mediana << std::endl;
 	return 0;
 }
-double Mediana(int ND[], int n){
+double Mediana(int *ND, int n, int p){
 	int laik;
 	double mediana;
 	for(int i = 1; i <= n; i++){
