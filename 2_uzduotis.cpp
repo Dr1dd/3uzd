d0f725d3c4181204862fe7cd25de23ce4f6df5b0
Laurynas Leišis <lauris.leisis@gmail.com>
2019-02-16 13:50:31 +0200

    0.2 versija tik be rūšiavimo

--- 2_uzduotis.cpp
+++ 2_uzduotis.cpp
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <cctype>
+#include <fstream>
 struct Studentai{
 	std::string fname, lname;
 	double egzGal;
@@ -16,18 +17,32 @@ struct Studentai{
      
 Studentai Informacija();
 void Spausdinti(const std::vector<Studentai> Studentai, std::string tipas, int studSkaic);
-double Mediana(Studentai &Stud, int n, int p);
+void Spausdinti(const std::vector<Studentai> Studentai);
+double Mediana(Studentai Stud, int n, int p);
 std::string Tikrinti(std::string tekstas);
 double TikrintiSkaicius(std::string tekstas);
+void Skaitymas(std::vector<Studentai> &StudentuInfo);
 int main(){
 	srand(time(NULL));
+	std::string pradinis;
+	std::vector<Studentai> StudentuInfo;
+	std::cout << "Skaitymas is failo ar ivestis? (1 = Skaitymas, 0=Ivestis)" << std::endl;
+	std::cin >> pradinis;
+		while(pradinis != "1" && pradinis !="0"){
+			std::cout <<"Ivestis neteisinga, bandykite dar karta ivesdami 1 ar 0" << std::endl;
+			std::cin >> pradinis;
+		}
+	if(pradinis == "1"){
+			Skaitymas(StudentuInfo);
+			Spausdinti(StudentuInfo);
+		}
+	else{
 	int studSkaic;
 	std::string tekstas;
 	std::cout <<"Iveskite studentu skaiciu" << std::endl;
 	std::cin.sync();
 	studSkaic = TikrintiSkaicius(tekstas);
 	
-	std::vector<Studentai> StudentuInfo;
 	for(int i = 0 ; i< studSkaic; i++) StudentuInfo.push_back(Informacija());
 	
 	
@@ -40,7 +55,7 @@ int main(){
 		std::cin >> tipas;
 }
 	Spausdinti(StudentuInfo, tipas, studSkaic);
-	
+}
 	return 0;
 }
 void Spausdinti(const std::vector<Studentai> Studentai, std::string tipas, int studSkaic){
@@ -52,7 +67,7 @@ void Spausdinti(const std::vector<Studentai> Studentai, std::string tipas, int s
 }
 	if(tipas == "Vid"){
 	std::cout <<std::left <<std::setw(7+ilgiausiaP)<<"Pavarde" <<std::left << std::setw(7+ilgiausiasV) << "Vardas" <<std::left << std::setw(19) <<"Galutinis (Vid.)" <<std::endl;
-	std::cout <<std::setfill('-') << std::setw(60) << "-"<< std::endl;
+	std::cout <<std::setfill('-') << std::setw(14+ilgiausiaP+ilgiausiasV+19) << "-"<< std::endl;
 	std::cout << std::setfill(' ');
 	for(int nuo = 0; nuo < studSkaic; nuo++){
 		std::cout <<std::left <<std::setw(7+ilgiausiaP)<< Studentai[nuo].lname << std::left << std::setw(7+ilgiausiasV)  << Studentai[nuo].fname <<std::left << std::setw(19) << std::setprecision(2) <<std::fixed << Studentai[nuo].egzGal << std::endl;
@@ -60,13 +75,33 @@ void Spausdinti(const std::vector<Studentai> Studentai, std::string tipas, int s
 	}
 	else{
 	std::cout <<std::left <<std::setw(7+ilgiausiaP)<<"Pavarde" <<std::left << std::setw(7+ilgiausiasV) << "Vardas" <<std::left << std::setw(19) << "Galutinis (Med.)" <<std::endl;
-	std::cout <<std::setfill('-') << std::setw(60) << "-"<< std::endl;
+	std::cout <<std::setfill('-') << std::setw(14+ilgiausiaP+ilgiausiasV+19) << "-"<< std::endl;
 	std::cout << std::setfill(' ');
 	for(int nuo1 = 0; nuo1 < studSkaic; nuo1++){
 		std::cout <<std::left <<std::setw(7+ilgiausiaP)<< Studentai[nuo1].lname << std::left << std::setw(7+ilgiausiasV)  << Studentai[nuo1].fname <<std::left << std::setw(19) << std::setprecision(2) <<std::fixed << Studentai[nuo1].mediana << std::endl;
 	}	
 	}
 	
+}
+void Spausdinti(const std::vector<Studentai> Studentai){
+	int studSkaic = 0;
+	for(int z=0; z<Studentai.size(); z++){
+		studSkaic++;
+	}
+	int ilg;
+	int ilgiausiasV = 0;
+	int ilgiausiaP = 0;
+	for(int i = 0; i < studSkaic; i++) {	
+    if(Studentai[i].fname.size() > ilgiausiasV) ilgiausiasV = Studentai[i].fname.size();
+    if(Studentai[i].lname.size() > ilgiausiaP) ilgiausiaP = Studentai[i].lname.size();
+}
+	std::cout <<std::left <<std::setw(7+ilgiausiaP)<<"Pavarde" <<std::left << std::setw(7+ilgiausiasV) << "Vardas" <<std::left << std::setw(19) <<"Galutinis (Vid.)" <<std::left << std::setw(19) <<"Galutinis (Med.)" <<std::endl;
+	std::cout <<std::setfill('-') << std::setw(14+ilgiausiaP+ilgiausiasV+38) << "-"<< std::endl;
+	std::cout << std::setfill(' ');
+	for(int nuo = 0; nuo < studSkaic; nuo++){
+		std::cout <<std::left <<std::setw(7+ilgiausiaP)<< Studentai[nuo].lname << std::left << std::setw(7+ilgiausiasV)  << Studentai[nuo].fname <<std::left << std::setw(19) << std::setprecision(2) <<std::fixed << Studentai[nuo].egzGal << std::setw(19) << std::setprecision(2) <<std::fixed << Studentai[nuo].mediana<< std::endl;
+	}
+
 }
 Studentai Informacija(){
 	std::string vardas, pavarde;
@@ -98,7 +133,7 @@ Studentai Informacija(){
 	bool gen = false;
 	std::string generuoti;
 	int randomSkaic;
-	std::cout <<"Iveskite kiek namu darbu buvo atlikta (Bet koks simbolis isskyrus skaicius = ivedimo baigimas irasius 0)" << std::endl;
+	std::cout <<"Iveskite kiek namu darbu buvo atlikta (Bet koks simbolis isskyrus skaicius = galesite baigti kai irasysite - 0)" << std::endl;
 	 std::cin.sync();
 	 std::getline(std::cin, ivestis);
 	 std::stringstream s(ivestis);
@@ -181,8 +216,10 @@ Studentai Informacija(){
 return Stud;
 }
 
-double Mediana(Studentai &Stud, int n, int p){
+double Mediana(Studentai Stud, int n, int p){
 	double mediana;
+	int f = 0;
+
 	std::sort(Stud.ND.begin(), Stud.ND.end());
 
 	if(n!=0){
@@ -255,5 +292,56 @@ double TikrintiSkaicius(std::string tekstas){
     }while(!valid);
     return std::stoi(tekstas);
 }
-
+void Skaitymas(std::vector<Studentai> &StudentuInfo){
+	std::string a;
+	std::string b;
+	int skaicius = 0;
+	int elSkaic = 0;
+	int ndSkaic;
+	double egz;
+	int p = 0;
+	Studentai Stud;
+	double vidurkis;
+	double galutinis;
+	double mediana;
+	int suma = 0;
+	
+	std::ifstream fd;
+	fd.open("kursiokai.txt");
+	if (!fd.is_open())
+    {
+       std::cout <<"Nepavyko atidaryti failo" << std::endl;
+    }
+    else{
+	while(std::getline(fd, a)) skaicius++;
+	fd.clear();
+	fd.seekg(0, std::ios::beg);
+	while(!fd.eof()){
+		fd >> a;
+		elSkaic++;
+		if(a == "Egzaminas") break;
+	}
+	ndSkaic = elSkaic - 3;
+	//fd.ignore(1000, '\n');
+	for(int i = 0; i < skaicius-1; i++){
+		for(int j = 0; j < elSkaic; j++){
+			fd >> b;
+			if(j == 0) Stud.lname = b;
+			if(j == 1) Stud.fname = b;
+			if(j == elSkaic-1) egz = std::stoi(b);
+			if(j>1 && j < elSkaic-1) {
+			Stud.ND.push_back(std::stoi(b));	
+			}
+		}
+	vidurkis = (double)suma/ndSkaic;
+	galutinis = 0.4 * vidurkis + 0.6*egz;
+	Stud.egzGal = galutinis;
+	mediana = Mediana(Stud, ndSkaic, p);
+	Stud.mediana = 0.4 * mediana + 0.6*egz;
+	StudentuInfo.push_back(Stud);
+	Stud.ND.clear();
+}
+	fd.close();
+}
+}
 
